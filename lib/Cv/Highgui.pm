65ae04ba65985fd85c29828b9ec56d781c5f9ef5
KUBO, Koichi <k@obuk.org>
2013-09-07 09:07:39 +0900

    add README

--- lib/Cv/Highgui.pm
+++ lib/Cv/Highgui.pm
@@ -9,7 +9,6 @@ Cv::Highgui - Cv extension for OpenCV High-level GUI and Media I/O (highgui)
 =head1 SYNOPSIS
 
   use Cv::Highgui;
-  blah blah blah
 
 =cut
 
@@ -43,12 +42,6 @@ XSLoader::load('Cv::Highgui', $VERSION);
 
 =head1 DESCRIPTION
 
-Stub documentation for Cv::Highgui, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
 =head2 METHOD
 
 =over
@@ -65,7 +58,6 @@ Blah blah blah.
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head2 EXPORT
 
@@ -73,14 +65,7 @@ None by default.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<Cv>
 
 =head1 AUTHOR
 
@@ -90,9 +75,7 @@ Koichi Kubo, E<lt>obuk@obuk.orgE<gt>
 
 Copyright (C) 2013 by Koichi Kubo
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.14.2 or,
-at your option, any later version of Perl 5 you may have available.
-
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself.
 
 =cut
